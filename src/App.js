3f1b9389b4b609bc074944dc2d8f96050fcce8d9
renjiroazhar <renjiroazhar@gmail.com>
2018-11-03 14:28:51 +0700

    PAPAGE SURAPAGE

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import LoginPage from "./Loginpage/LoginPage";
+import MobileApp from './Routes/MobileApp';
+import WebApp from './Routes/MobileApp';
+import { isMobile } from "react-device-detect";
 class App extends Component {
+  state = {
+    login: null
+  }
+
+  updateLogin = () => {
+    this.setState({
+      login: true
+    })
+  }
+
+  updateLogout = () => {
+    this.setState({
+      login: false
+    })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+    if (!(sessionStorage.getItem('loginState'))) {
+      return <LoginPage updateLogin={
+        this.updateLogin
+      }
+      />;
+    }
+
+    if (isMobile) {
+      return <MobileApp updateLogout={
+        this.updateLogout
+      }
+      />;
+    }
+
+    return <WebApp updateLogout={
+      this.updateLogout
+    }
+    />;
   }
 }
 
